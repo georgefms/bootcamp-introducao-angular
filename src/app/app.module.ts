4aec4fc7ed4d73ca1cd99a2d5a4412366a89bd5e
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-08-02 18:28:34 -0300

    Feat: Creating a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/courses-list.component';
 import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
