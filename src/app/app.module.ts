9ada87fa7d67ca34d1c12ba16690e6935908f77d
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-08-03 20:19:52 -0300

    Refactor: creating a course module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,28 +11,19 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './error-404/error-404.component';
 import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
-    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    CourseModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
