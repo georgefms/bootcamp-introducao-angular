fc4801b748cf35dbb522d8333e5cdac9f990f603
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-08-03 20:34:45 -0300

    Refactor: creating a model to pipes and starComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/courses-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './error-404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
 import { CourseModule } from './courses/course.module';
 
 @NgModule({
