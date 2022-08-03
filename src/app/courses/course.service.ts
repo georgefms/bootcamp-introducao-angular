34c8b9f3f82a6fa2d5876d8c905b185cf2fefbe0
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-08-03 19:57:01 -0300

    Feat: implementing delete button

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -28,7 +28,12 @@ export class CourseService{
   } else {
     return this.httpClient.post<Course>(`${this.courseUrl}`, course)
   }
-}
+  }
+  
+  deleteById(id: number): Observable<any>{
+    return this.httpClient.delete<any>(`${this.courseUrl}/${id}`)
+  }
+
 }
 var COURSES: Course[] = [
   {
