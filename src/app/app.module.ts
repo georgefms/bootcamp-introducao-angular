674e6c854dcb3ed65e6289165dfdd0849f32103e
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-08-03 21:06:36 -0300

    Refactor: adding error404component to core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    CoreModule,
     CourseModule,
     HttpClientModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
